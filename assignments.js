585023fe2b90d020c30416800da7b88309ced200
Rashawn <arctitan78@gmail.com>
2022-08-15 16:44:21 -0400

    assignment 1 - values variables

---
+++ assignments.js
@@ -0,0 +1,13 @@
+// Assignment: Values and Variables
+console.log('-----Begin: Assignment 1: Values and Variables-----');
+
+// declare variables
+let country = 'United States of America';
+let continent = 'North America';
+let population = '350 million';
+
+//log the values of the variables to the console
+console.log('I live in the', country, '.');
+console.log('The', country, 'is in', continent, '.');
+console.log('The', country, 'has a population of', population, '.');
+console.log('-----End: Assignment 1: Values and Variables-----');
