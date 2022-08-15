7e6274ab2afa7e814d0791e77b3dba334775c87a
Rashawn <arctitan78@gmail.com>
2022-08-15 16:43:46 -0400

    practice lesson on variables

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
+// less on variables and values
+
+// display some output in the browser
 console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+console.log(23);
+
+// declare a variable
+let firstName = "Jonas"
+console.log("The first name is", firstName);
+console.log("Repeat the first name", firstName);
+
+// conventions and rules for naming variables
+console.log('use camelCase for variable names');
+console.log('firstName lastName eyeColor');
+
+// convention and rules for function 
+console.log('use camelCase for function names');
+console.log('myFunction');
+
+// do not start a variable name with a number
+console.log('do not start a variable with a number');
+console.log('This generates an error message: 3years = 3');
+
+// variable can only contain the following:
+console.log('abcABC0-9, __ , $ are permitted in variable names');
+
+// mathematical constants are written in UPPERCASE
+console.log('mathematical constants are written in UPPERCASE');
+console.log('let PI = 3.14159');
+let PI = 3.14159
