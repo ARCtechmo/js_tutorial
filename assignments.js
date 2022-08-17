989e061f72207e6fd5bb6d8d596015d11d6610b4
Rashawn <arctitan78@gmail.com>
2022-08-17 19:33:41 -0400

    completed data types assignment

--- assignments.js
+++ assignments.js
@@ -11,3 +11,13 @@ console.log('I live in the', country, '.');
 console.log('The', country, 'is in', continent, '.');
 console.log('The', country, 'has a population of', population, '.');
 console.log('-----End: Assignment 1: Values and Variables-----');
+
+// Assignment: Data Types
+console.log('-----Begin: Assignment 2: Data Types-----');
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+console.log('-----End: Assignment 2: Data Types-----');
