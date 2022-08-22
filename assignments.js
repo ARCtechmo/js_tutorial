e92f6cfac0f5dd3812cf9c956ad5e597257e6dd5
Rashawn <arctitan78@gmail.com>
2022-08-22 19:01:48 -0400

    added assignments on variables and basic operators

--- assignments.js
+++ assignments.js
@@ -21,3 +21,34 @@ console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
 console.log('-----End: Assignment 2: Data Types-----');
+
+// Assignment: Declare Variables
+console.log('\n------Begin Assignment 3: Declare Variables-------');
+console.log('Reassign the variable "language."');
+language = 'English'
+console.log('The language of my country is', language + '.');
+
+console.log('Make some constant variables.');
+const numStates = 50
+console.log('There are', numStates, 'states in the', country + '.')
+console.log('------End Assignment 3: Declare Variables-------');
+
+// Assignment: Basic Operators
+console.log('\n-------Begin Assignment 4: Basic Operators--------');
+population = 350000000
+let halfPop = (population / 2);
+console.log('Half the population:', halfPop);
+
+halfPop++
+console.log('Increase the half-population by 1 using halfPopulation++:', halfPop);
+
+let popFinland = 6000000
+console.log('Is Finland\'s population less than the U.S?', popFinland < halfPop);
+
+avgPopulation = 33000000
+console.log('Is the avg. pop. < half the U.S?', avgPopulation < halfPop);
+
+const description = 'The ' + country + ' is in ' + continent + ' and its ' + population + ' people speak ' + language + '.';
+console.log(description);
+
+console.log('\n-------End Assignment 4: Basic Operators--------');
