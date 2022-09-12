bbcfa4dd290e3a307363dd0feedd29d8029cb8d2
Rashawn <arctitan78@gmail.com>
2022-09-12 15:52:14 -0400

    added the swtich statement and conditional (ternary) oeprator assignments

--- assignments.js
+++ assignments.js
@@ -100,3 +100,31 @@ if ((questionPop > 25000000) && (questionIsland === 'N' | 'n') && (questionLang 
 } else {
     console.log(`You should not live in ${questionCountry}.`)
 };
+
+// Assignment: The Switch Statement
+const language = 'chinese';
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('Most number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers.');
+        break;
+    case english:
+        console.log('3rd place');
+        break;
+    case hindi:
+        console.log('Number 4');
+        break;
+    case arabic:
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too');
+}
+
+// Assignment: The Conditional (Ternary) Operator
+let country = prompt()
+let population = Number(prompt());
+population > 33000000 ? console.log(`${country}'s population is above `) : console.log(`${country}'s population is below.`)
