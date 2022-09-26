0f0de2a273ff733856b02342351ca73524f5ba6f
ARCtechmo <arctitan78@gmail.com>
2022-09-26 14:50:40 -0400

    added lessons on strict mode and functions

---
+++ script2.js
@@ -0,0 +1,40 @@
+// activate strict mode
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+// intentional error incorrect variable name
+// this will output an error in 'use strict' mode 
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive : D');
+
+// reserved words are applied in 'use strict' mode
+// if you use a reserved word it will generate an error
+// const interface = 'Audio';
+
+
+// functions - call, run, or invoke a function
+function logger(){
+    console.log('My name is John Doe');
+};
+logger();
+
+// function parameters pass data into a function
+// in the example below 'apples' and 'oranges' are the parameters
+// we passethe integers 5 and 0 into the parameters 'apples' and 'oranges', respectively
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+};
+
+// assigne the function to a variable
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+
+// get different values for the function
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+*/
+
