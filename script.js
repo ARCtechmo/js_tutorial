fa45c7347d9c17938fa0d97be65e1e9f625986d1
ARCtechmo <arctitan78@gmail.com>
2023-08-23 17:52:09 -0400

    more practice on array methods

--- script.js
+++ script.js
@@ -433,4 +433,57 @@ function multiplyByTwo(value) {
 const numbers3 = [1, 2, 3, 4, 5];
 const doubleNumbers3 = Array.from(numbers3, multiplyByTwo);
 console.log(doubleNumbers3); // output: [ 2, 4, 6, 8, 10 ]
+
+console.log("print");
+
+// Array methods practice
+// calculate how much has been depostied to the
+const bankDepositSum = accounts
+	.flatMap((acc) => acc.movements)
+	.filter((mov) => mov > 0)
+	.reduce((sum, cur) => sum + cur, 0);
+console.log(bankDepositSum);
+
+// Array methods practice
+// number of deposits >= 1000
+const numDeposits1000 = accounts
+	.flatMap((acc) => acc.movements)
+	.filter((mov) => mov >= 1000).length;
+console.log(numDeposits1000);
+
+// Array methods practice - reduce method
+// number of deposits >= 1000
+const numDeposits1000_2 = accounts
+	.flatMap((acc) => acc.movements)
+	.reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+console.log(numDeposits1000_2);
+
+// Array methods practice - reduce method
+// create an object that contains a sum of deposits and withdrawals
+const sums = accounts
+	.flatMap((acc) => acc.movements)
+	.reduce(
+		(sums, cur) => {
+			cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+			return sums;
+		},
+		{deposits: 0, withdrawals: 0}
+	);
+console.log(sums);
+
+// Array methods practice -
+// convert a string to title case
+const convertTitleCase = function (title) {
+	const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+	const exceptions = ["a", "an", "and", "the", "but", "or", "on", "in", "with"];
+	const titleCase = title
+		.toLowerCase()
+		.split(" ")
+		.map((word) => (exceptions.includes(word) ? word : capitalize(word)))
+		.join(" ");
+	return capitalize(titleCase);
+};
+console.log(convertTitleCase("this is a LONG title but not too long"));
+console.log(convertTitleCase("and here is another title with an EXAMPLE"));
+
 //////////////////////////////////////////////////////////////////////////////////////
